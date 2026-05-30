843bc1ddf8a4d570a46e5a227664aae05098a442
VarvaraSed <varvarasedyshevafgh@gmail.com>
2026-05-30 23:36:09 +0300

    Update main.cpp_2

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 // Copyright 2026 NNTU-CS
 #include <iostream>
+#include <string>
 
 #include "bst.h"
-
 
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
