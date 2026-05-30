265b0bcbf32ad5742a65c8fce9e30815e05c4a2a
VarvaraSed <varvarasedyshevafgh@gmail.com>
2026-05-30 23:20:58 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2026 NNTU-CS
+#include <iostream>
+
 #include "bst.h"
 
+
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
+    BST<std::string> tree;
+    makeTree(tree, "src/war_peace.txt");
+
+    std::cout << "Unique words: " << tree.size() << std::endl;
+    std::cout << "Tree depth: " << tree.depth() << std::endl;
+    std::cout << std::endl;
+    printFreq(tree);
     return 0;
 }
